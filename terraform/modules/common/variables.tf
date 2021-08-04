b1e7075024238eef072a34e77c1a6cabb2382524
toshi <toshi@toshi.click>
2021-08-04 21:28:49 +0900

    add terraform_remote_state

---
+++ terraform/modules/common/variables.tf
@@ -0,0 +1,10 @@
+# S3にあるtfstateを使う設定
+data "terraform_remote_state" "common" {
+  backend = "s3"
+
+  config {
+    bucket = "toshi-terraform-state"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
