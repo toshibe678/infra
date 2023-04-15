f256539fe60e81efcc561e9fc349ca8c096976d5
toshi <toshi@toshi.click>
2023-04-15 23:28:57 +0900

    Terraformのイメージでtokenの環境変数を大文字に変更

--- docker/terraform/entrypoint.sh
+++ docker/terraform/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -z "${terraform_token}" ]]; then
+if [[ -z "${TERRAFORM_TOKEN}" ]]; then
   echo "no terraform_token"
 else
-  sed -i -e "s/TOKEN_KEY/${terraform_token}/g" /root/.terraformrc
+  sed -i -e "s/TOKEN_KEY/${TERRAFORM_TOKEN}/g" /root/.terraformrc
 fi
 
 exec "$@"
