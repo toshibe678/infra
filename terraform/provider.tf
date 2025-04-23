fc1fd4d80f53101620bf175b06726e544962b386
toshi <toshi@toshi.click>
2025-04-23 20:04:03 +0900

    terraform: .gitignoreに.lock.hclファイルを追加し、provider.tfを新規作成

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = ">= 1.8.0"
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/aws/latest
-    aws = ">= 5.75.0"
+    aws = ">= 5.94.0"
   }
 }
+provider "cloudflare" {}
